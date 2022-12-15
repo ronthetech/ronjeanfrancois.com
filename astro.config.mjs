7f58fbfd7fcfc393606adef7acd640695dd9da90
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-12-15 00:18:17 -0500

    update astro config with tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,20 @@
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-import prefetch from "@astrojs/prefetch"
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.ronjeanfrancois.com/",
-	markdown: {
-		shikiConfig: {
-			theme: "github-dark",
-		},
-	},
-	integrations: [mdx(), sitemap(), prefetch()],
-})
+  site: "https://www.ronjeanfrancois.com/",
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark"
+    }
+  },
+  integrations: [mdx(), sitemap(), prefetch(), tailwind()]
+});
