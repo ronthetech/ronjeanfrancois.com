f8c7cbcb83b9aa1058e1eebcf612b8a1584d96ad
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2022-12-15 17:38:23 -0500

    refactor: organize imports, remove comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
-
-// https://astro.build/config
-import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx"
+import prefetch from "@astrojs/prefetch"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.ronjeanfrancois.com/",
-  markdown: {
-    shikiConfig: {
-      theme: "github-dark"
-    }
-  },
-  integrations: [mdx(), sitemap(), prefetch(), tailwind()]
-});
+	site: "https://www.ronjeanfrancois.com/",
+	integrations: [mdx(), sitemap(), prefetch(), tailwind()],
+})
