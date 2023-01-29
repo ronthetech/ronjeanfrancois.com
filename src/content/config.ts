7aa28a444963d8aa643527a5a91b543306c87a1b
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-29 16:54:09 -0500

    add slug

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ export const collections = {
       description: z.string(),
       date: z.date().transform((str) => new Date(str)),
       pubDate: z.string(),
+      slug: z.string().optional(),
       heroImage: z.string().optional(),
       alt: z.string().optional(),
       // draft: z.boolean().default(false), if its not a draft, its publishable
