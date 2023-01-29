0a9abf6514c1ef27d637c21068d97cd5190d02ef
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-01-29 12:54:00 -0500

    delete .astro

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+import { defineCollection, z } from "astro:content"
+
+export const collections = {
+  blog: defineCollection({
+    schema: z.object({
+      layout: z.string(),
+      title: z.string(),
+      description: z.string(),
+      date: z.date().transform((str) => new Date(str)),
+      pubDate: z.string(),
+      heroImage: z.string().optional(),
+      alt: z.string().optional(),
+      // draft: z.boolean().default(false), if its not a draft, its publishable
+      category: z.string(),
+      tags: z.array(z.string()).optional(),
+    }),
+  }),
+}
